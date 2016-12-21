ae1b3b35f2ae1849a5185d1fd04adf81f1b9bcfa
Mitch Woodard <woodardcommamitch@gmail.com>
2016-12-21 09:54:47 -0600

    unsorted tree creation working

--- lib/Tree.rb
+++ lib/Tree.rb
@@ -1,9 +1,10 @@
 class Node
 	attr_accessor :value, :left, :right
-	def initialize(value = nil, left = nil, right = nil)
+	def initialize(value = nil, left = nil, right = nil, parent = nil)
 		@value = value
 		@left= left
 		@right = right
+		@parent = parent
 	end
 end
 
@@ -13,12 +14,47 @@ class Tree
 		@root = nil
 	end
 	
-	def build_tree(ary, first = 0, last = ary.length)		
+	def build_tree(ary)
+		stack = []
+		@root = Node.new(ary.pop)
+		
+		while ary.length>0
+			add_node(ary.pop)
+		end		
+	end
+	
+	def add_node(value)
+		current_node = @root
+		correct_position = false	
+		
+		while !correct_position
+			if value < current_node.value && current_node.left == nil
+				puts "Adding node (#{value})to left side of #{current_node.value}"
+				current_node.left = Node.new(value)
+				correct_position = true
+			elsif value < current_node.value && current_node.left != nil
+				#puts "Dropping from #{current_node.value} to #{current_node.left.value}"
+				current_node = current_node.left
+			elsif value > current_node.value && current_node.right == nil
+				puts "Adding node (#{value})to right side of #{current_node.value}"
+				current_node.right = Node.new(value)
+				correct_position = true
+			elsif value > current_node.value && current_node.right != nil
+				#puts "Dropping from #{current_node.value} to #{current_node.right.value}"
+				current_node = current_node.right
+			else
+				puts "nothing done"
+			end
+		end
+		#puts current_node.value
+		
+	end
+	
+	def build_tree_rec(ary, first = 0, last = ary.length)		
 		if first>last
 			return nil
 		end
 		mid = (first+last)/2
-		#puts ary[mid]
 		
 		branch = Node.new(ary[mid])
 		if @root == nil
@@ -28,14 +64,17 @@ class Tree
 		branch.left=build_tree(ary,first, mid-1)
 		branch.right=build_tree(ary, mid+1, last)
 		return branch
-	end
+	end	
 end
                  #[1, 3, 4, 4, 5, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345]
-test_array = [1, 23, 8, 9, 4, 3, 7 , 67, 6345, 324, 5]
-test_array = [1, 3, 4, 5, 6, 7, 8, 9, 23, 67, 324, 6345]
+test_array = [23, 8, 9, 4,6345, 3, 7, 2 , 67,  324]
+#test_array = [1, 3, 4]#, 5, 6, 7, 8, 9, 23, 67, 324, 6345]
 test_tree = Tree.new
 
-test_tree.build_tree(test_array.sort)
+test_tree.build_tree(test_array)
 
-
-puts test_tree.root.right.right.left.value
+puts "Root: #{test_tree.root.value}"
+puts "Left of root : #{test_tree.root.left.value}"
+#puts "Left of #{test_tree.root.left.left.value}: #{test_tree.root.left.left.left.value}"
+puts "Left of #{test_tree.root.left.left.right.left.value}: #{test_tree.root.left.left.right.right.right.value} "
+#puts "Right of #{test_tree.root.left.left.right.value}: #{test_tree.root.left.left.right..right.value}"
--- trash.rb
+++ trash.rb
@@ -1,69 +1,8 @@
 
+test = "0"
+test1= 0
 
-
-class Node
-	attr_accessor :value, :left, :right, :parent
-	def initialize(value = nil, left = nil, right = nil, parent = nil)
-		@value = value
-		@left= left
-		@right = right
-		@parent = parent
-	end
-end
-
-class Tree
-	attr_accessor :tree_q
-	def initialize
-		@root = nil
-		@tree_q = []
-	end
-	
-	def build_tree(ary)		
-		
-		n = ary.length		
-		mid = ary[n/2]
-		current_node = Node.new(mid)
-		tree_q.push(current_node)
-		ary_a = ary[0..n/2-1]
-		mid_a = ary_a[ary_a.length/2]
-		#current_node.left.value = mid_a
-		ary_b = ary[n/2+1..n]		
-		mid_b = ary_b[ary_b.length/2]
-		puts "#{tree_q[tree_q.length-1].value} #{tree_q[tree_q.length-1].left}"
-		
-		if @root == nil
-			@root = mid
-		end
-		
-		if n<=1	
-			tree_q.push ary[0]
-			return ary
-		end		
-		
-		ary_a = ary[0..n/2-1]
-		mid_a = ary_a[ary_a.length/2]
-		#current_node.left.value = mid_a
-		ary_b = ary[n/2+1..n]		
-		mid_b = ary_b[ary_b.length/2]
-		#puts "#{tree_q[tree_q.length-1].value} #{tree_q[tree_q.length-1].left}"
-		
-		
-		
-		
-		#puts "Root: #{@root}"
-		#puts "Current Node: #{current_node}"
-		#puts ary_a.inspect
-		#puts ary_b.inspect
-	
-		
-		build_tree(ary_a)
-		build_tree(ary_b)		 
-	end
-end
-                 #[1, 3, 4, 4, 5, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345]
-test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 5]
-test_array = test_array.sort
-
-test_tree = Tree.new
-test_tree.build_tree(test_array)
+puts test.class
+puts test1.class
+puts test.to_i.class
 
